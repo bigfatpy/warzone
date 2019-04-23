351cefc1f22554e07bb9174d91606ed397736a56
Marcelo Barbosa <mabarbo@microsoft.com>
2019-04-23 10:00:56 +0300

    fixing the unittest

--- tests/test_reverse_words.py
+++ tests/test_reverse_words.py
@@ -6,7 +6,7 @@ class Test(unittest.TestCase):
     result = reverse_words(value='I am a software engineer. But,, I am also an operations engi.neer.')
 
     def test_result(self):
-        self.assertEqual('I ma a erawtfos reenigne. tuB,, I ma osla na snoitarepo igne.reen.', self.result)
+        self.assertEqual(self.result, 'I ma a erawtfos reenigne. tuB,, I ma osla na snoitarepo igne.reen.')
 
 
 if __name__ == "__main__":
